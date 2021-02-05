c40329a49c478868ba1a5f7770bb3226a6fb95d2
Mubashir Jamali <mubashirjamali101@gmail.com>
2021-02-05 22:10:38 +0500

    added option to leave input empty as doing of ? sign, both will work same to find that parameter

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const log = console.log;
 
-let n1 = prompt("a="), n2 = prompt("b="), n3 = prompt("c="); // taking initial user input, enter a number as value or a ? sign to find it that value
+let n1 = prompt("a = "),
+    n2 = prompt("b = "),
+    n3 = prompt("c = "); // taking initial user input, enter a number as value or a ? sign to find it that value
 
-function pyth(a = 1, b = 1, c = 1) {//the main function which uses  pythagores theorem to find the one of the arguments of the a, b & c
-    if(a == "?" && Number(b) && b != "?" && Number(c) && c != "?"){
-        b = Number(b)
-        c = Number(c)
-        a = Math.sqrt((c*c)-(b*b))
+function pyth(a = 1, b = 1, c = 1) { //the main function which uses  pythagores theorem to find the one of the arguments of the a, b & c
+    if (a == "" || a == "?" && b != "" && c != "" && Number(b) && b != " ? " && Number(c) && c != " ? ") {
+        b = Number(b);
+        c = Number(c);
+        a = Math.sqrt((c * c) - (b * b));
         log(`a = ${a}`)
-    }else if(b == "?" && Number(a) && a != "?" && Number(c) && c != "?"){
+    } else if (b == "" || b == "?" && Number(a) && a != "?" && Number(c) && c != "?") {
         a = Number(a)
         c = Number(c)
-        b = Math.sqrt((c*c)-(a*a))
+        b = Math.sqrt((c * c) - (a * a))
         log(`b = ${b}`)
-    }else if(c == "?" && Number(b) && b != "?" && Number(a) && a != "?"){
+    } else if (c == "" || c == "?" && b != "" && a != "" && Number(b) && b != "?" && Number(a) && a != "?") {
         b = Number(b)
         a = Number(a)
-        c = Math.sqrt((a*a)+(b*b))
+        c = Math.sqrt((a * a) + (b * b))
         log(`c = ${c}`)
-    }else if(Number(b)&& Number(a) && Number(c)){
+    } else if (Number(b) && Number(a) && Number(c)) {
 
         b = Number(b)
         c = Number(c)
         a = Number(a)
-        log(`${(c*c)}(c) = ${(a*a)}(a) + ${(b*b)}(b)`)
-    }else {
+        if ((a * a) + (b * b) == (c * c)) {
+            log(`${(c*c)}(c) = ${(a*a)}(a) + ${(b*b)}(b)`)
+        } else {
+            log(`ERROR: The entered parameter's doesn't satisfy Pythagores Theorem.`)
+        }
+    } else {
         log("Two arguments must be numbers and third should be \"?\" sign.")
     }
 
